a91a591f110de7cee266fc9e34fe2cf98d098660
Егор Колтышев <koltyshev.egor@yandex.ru>
2026-05-11 14:50:17 +0300

    added lab7 and removed serts

--- lab3/client.cpp
+++ lab3/client.cpp
@@ -2,8 +2,6 @@
 #include <string>
 #include <thread>
 #include <atomic>
-#include <mutex>
-#include <chrono>
 #include <cstring>
 
 #include <sys/socket.h>
@@ -40,7 +38,6 @@ static bool recvMessage(int fd, Message& msg) {
 static bool sendMessage(int fd, const Message& msg) {
     uint32_t len = ntohl(msg.length);
     if (len < 1) return false;
-
     uint32_t payloadLen = len - 1;
 
     if (!sendAll(fd, &msg.length, sizeof(msg.length))) return false;
@@ -51,243 +48,115 @@ static bool sendMessage(int fd, const Message& msg) {
     return true;
 }
 
-static int connectToServer(const std::string& ip, int port) {
-    int s = ::socket(AF_INET, SOCK_STREAM, 0);
-    if (s < 0) {
-        perror("socket");
-        return -1;
-    }
-
-    sockaddr_in addr{};
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(static_cast<uint16_t>(port));
-
-    if (inet_pton(AF_INET, ip.c_str(), &addr.sin_addr) != 1) {
-        std::cerr << "Bad ip\n";
-        ::close(s);
-        return -1;
-    }
-
-    if (::connect(s, (sockaddr*)&addr, sizeof(addr)) < 0) {
-        perror("connect");
-        ::close(s);
-        return -1;
-    }
-
-    return s;
-}
-
-static bool performHandshake(int s, const std::string& nick) {
-    Message hello{};
-    buildMessage(hello, MSG_HELLO, nick);
-
-    if (!sendMessage(s, hello)) {
-        std::cerr << "Failed to send HELLO\n";
-        return false;
-    }
-
-    Message welcome{};
-    if (!recvMessage(s, welcome) || welcome.type != MSG_WELCOME) {
-        std::cerr << "Expected WELCOME\n";
-        return false;
-    }
-
-    std::cout << payloadToString(welcome) << "\n";
-    return true;
-}
-
 int main(int argc, char** argv) {
     if (argc < 4) {
         std::cerr << "Usage: " << argv[0] << " <ip> <port> <nick>\n";
         return 1;
     }
-
     std::string ip = argv[1];
     int port = std::stoi(argv[2]);
     std::string nick = argv[3];
 
-    std::atomic<bool> running{true};
-    std::atomic<bool> connected{false};
-
-    int sock = -1;
-    std::mutex sockMutex;
-
-    auto safeCloseSocket = [&]() {
-        std::lock_guard<std::mutex> lock(sockMutex);
-        if (sock >= 0) {
-            ::shutdown(sock, SHUT_RDWR);
-            ::close(sock);
-            sock = -1;
-        }
-        connected.store(false);
-    };
-
-    auto sendCurrentMessage = [&](const Message& msg) -> bool {
-        std::lock_guard<std::mutex> lock(sockMutex);
-
-        if (sock < 0) {
-            return false;
-        }
-
-        if (!sendMessage(sock, msg)) {
-            ::shutdown(sock, SHUT_RDWR);
-            ::close(sock);
-            sock = -1;
-            connected.store(false);
-            return false;
-        }
-
-        return true;
-    };
-
-    auto connectAndHandshake = [&]() -> bool {
-        int newSock = connectToServer(ip, port);
-        if (newSock < 0) {
-            return false;
-        }
-
-        if (!performHandshake(newSock, nick)) {
-            ::close(newSock);
-            return false;
-        }
-
-        {
-            std::lock_guard<std::mutex> lock(sockMutex);
-            sock = newSock;
-        }
-
-        connected.store(true);
-        std::cout << "Connected\n";
-        return true;
-    };
-
-    while (running.load() && !connectAndHandshake()) {
-        std::cout << "Reconnect in 2 seconds...\n";
-        std::this_thread::sleep_for(std::chrono::seconds(2));
+    int s = ::socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0) {
+        perror("socket");
+        return 1;
     }
 
-    std::thread rx([&]() {
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(static_cast<uint16_t>(port));
+    if (inet_pton(AF_INET, ip.c_str(), &addr.sin_addr) != 1) {
+        std::cerr << "Bad ip\n";
+        ::close(s);
+        return 1;
+    }
+
+    if (::connect(s, (sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("connect");
+        ::close(s);
+        return 1;
+    }
+
+    std::cout << "Connected\n";
+
+    // HELLO
+    Message hello{};
+    buildMessage(hello, MSG_HELLO, nick);
+    if (!sendMessage(s, hello)) {
+        std::cerr << "Failed to send HELLO\n";
+        ::close(s);
+        return 1;
+    }
+
+    // WELCOME
+    Message welcome{};
+    if (!recvMessage(s, welcome) || welcome.type != MSG_WELCOME) {
+        std::cerr << "Expected WELCOME\n";
+        ::close(s);
+        return 1;
+    }
+    std::cout << payloadToString(welcome) << "\n";
+
+    std::atomic<bool> running{true};
+
+    // Поток приёма
+    std::thread rx([&]{
         while (running.load()) {
-            if (!connected.load()) {
-                std::this_thread::sleep_for(std::chrono::milliseconds(200));
-                continue;
-            }
-
-            int localSock = -1;
-            {
-                std::lock_guard<std::mutex> lock(sockMutex);
-                localSock = sock;
-            }
-
-            if (localSock < 0) {
-                connected.store(false);
-                continue;
-            }
-
             Message in{};
-            if (!recvMessage(localSock, in)) {
-                {
-                    std::lock_guard<std::mutex> lock(sockMutex);
-                    if (sock == localSock) {
-                        ::shutdown(sock, SHUT_RDWR);
-                        ::close(sock);
-                        sock = -1;
-                    }
-                }
-
-                connected.store(false);
-
-                if (!running.load()) {
-                    break;
-                }
-
-                std::cout << "\nConnection lost. Reconnecting...\n";
-
-                while (running.load() && !connected.load()) {
-                    if (connectAndHandshake()) {
-                        std::cout << "> " << std::flush;
-                        break;
-                    }
-                    std::cout << "Reconnect in 2 seconds...\n";
-                    std::this_thread::sleep_for(std::chrono::seconds(2));
-                }
-
-                continue;
+            if (!recvMessage(s, in)) {
+                running.store(false);
+                break;
             }
 
             if (in.type == MSG_TEXT) {
-                std::cout << "\n" << payloadToString(in) << "\n> " << std::flush;
+                std::cout << payloadToString(in) << "\n";
             } else if (in.type == MSG_PONG) {
-                std::cout << "\nPONG\n> " << std::flush;
+                std::cout << "PONG\n";
             } else if (in.type == MSG_BYE) {
-                std::cout << "\nServer closed connection\n";
-                connected.store(false);
-
-                {
-                    std::lock_guard<std::mutex> lock(sockMutex);
-                    if (sock == localSock) {
-                        ::shutdown(sock, SHUT_RDWR);
-                        ::close(sock);
-                        sock = -1;
-                    }
-                }
-
-                while (running.load() && !connected.load()) {
-                    if (connectAndHandshake()) {
-                        std::cout << "> " << std::flush;
-                        break;
-                    }
-                    std::cout << "Reconnect in 2 seconds...\n";
-                    std::this_thread::sleep_for(std::chrono::seconds(2));
-                }
+                running.store(false);
+                break;
             }
         }
     });
 
+    // Поток ввода и отправки в main
     std::string line;
     while (running.load()) {
         std::cout << "> " << std::flush;
-
         if (!std::getline(std::cin, line)) {
-            running.store(false);
+            // EOF
             break;
-        }
-
-        if (!connected.load()) {
-            std::cout << "Not connected right now\n";
-            continue;
         }
 
         if (line == "/ping") {
             Message ping{};
             buildMessage(ping, MSG_PING, "");
-
-            if (!sendCurrentMessage(ping)) {
-                std::cout << "Send failed\n";
+            if (!sendMessage(s, ping)) {
+                running.store(false);
+                break;
             }
         } else if (line == "/quit") {
             Message bye{};
             buildMessage(bye, MSG_BYE, "");
-            (void)sendCurrentMessage(bye);
-
+            (void)sendMessage(s, bye);
             running.store(false);
             break;
         } else {
             Message txt{};
             buildMessage(txt, MSG_TEXT, line);
-
-            if (!sendCurrentMessage(txt)) {
-                std::cout << "Send failed\n";
+            if (!sendMessage(s, txt)) {
+                running.store(false);
+                break;
             }
         }
     }
 
     running.store(false);
-    safeCloseSocket();
+    ::shutdown(s, SHUT_RDWR);
+    ::close(s);
 
-    if (rx.joinable()) {
-        rx.join();
-    }
+    if (rx.joinable()) rx.join();
 
     std::cout << "Disconnected\n";
     return 0;
